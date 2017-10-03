bdf41c7e376396f8a770b692cee30fdad8b5bbfb
rlopez178 <rlopez178@yahoo.com>
2017-10-02 21:55:54 -0400

    End of lesson 2

--- Smack App/Controllers/ChannelVC.swift
+++ Smack App/Controllers/ChannelVC.swift
@@ -10,26 +10,15 @@ import UIKit
 
 class ChannelVC: UIViewController {
 
+    
+    
+    //MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Allows for the side menu to show most of it's self minus 60.
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Smack App/Controllers/ChatVC.swift
+++ Smack App/Controllers/ChatVC.swift
@@ -9,27 +9,25 @@
 import UIKit
 
 class ChatVC: UIViewController {
-
+    
+    // MARK: Outlets
+    
+    // Notice that this is a outlet not an action
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Because the menuBtn isn't an action, we have to give it an action manually. I'm guessing we have to do it like this because of the way SWReveal is coded.
+        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        // These add the drag and tap to get out gestures, to add a little pizaz
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
